bbae2b11aa0427dbfe6747c13b4f1075802cf644
meet <meetsolanki7974@gmail.com>
2023-10-27 19:45:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const API_KEY = "00490507b85c4d94b7bc92f81043203e"
+const url = "https://newsapi.org/v2/everything?q="
+
+window.addEventListener('load', fetchNews('india'));
+
+async function fetchNews(query){
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`)
+    const data = await res.json()
+    console.log(data)
+    bindData(data.articles);
+}
+
+function bindData(articles){
+    const cardsContainer = document.getElementById("cards-container")
+    const newsCardTemplate = document.getElementById("template-news-card")
+ cardsContainer.innerHTML="";
+
+    articles.forEach(article => {
+        if(!article.urlToImage) return ; 
+        const cardClone = newsCardTemplate.content.cloneNode(true);        
+        fillDataInCard(cardClone,article);
+        cardsContainer.appendChild(cardClone)
+    });
+}
+
+
+function fillDataInCard(cardClone,article){
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newssource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage
+    newsTitle.innerHTML = article.title
+    newsDesc.innerHTML = article.description
+
+    const date = new Date(article.publishedAt).toLocaleString('en-US',{
+        timeZone : "Asia/jakarta"
+    });
+
+    newssource.innerHTML = `${article.source.name} : ${date}`
+    cardClone.firstElementChild.addEventListener('click', ()=>{
+        window.open(article.url,"_blank");
+    })
+}
+
+
+let curselectednav = null;
+
+function onnavitem(id){
+
+    fetchNews(id)
+
+    const navitem = document.getElementById(id)
+    curselectednav?.classList.remove('active')
+    curselectednav = navitem
+    curselectednav.classList.add('active')
+
+
+}
+
+const serchbutton = document.getElementById('serch-button')
+const serchtext = document.getElementById('serch-text')
+
+serchbutton.addEventListener('click' , ()=>{
+const query = serchtext.value;    
+if(!query) return;
+fetchNews(query)
+curselectednav?.classList.remove('active')
+curselectednav = null;
+
+})
+
+function reload(){
+    window.location.reload();
+}
